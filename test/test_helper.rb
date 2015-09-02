04222b0adc496dc281f8afb9040358b00ca54581
rudiments <fb53ice4j@gmail.com>
2015-09-02 07:01:57 +0000

    create contactpage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+#これをすると、テストの確認画面がカラフルになるよ
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
